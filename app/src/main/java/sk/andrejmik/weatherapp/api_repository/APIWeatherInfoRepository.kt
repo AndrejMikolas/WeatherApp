c18de02a8c3882cd6b2fb58d99449b3f45a29d8d
Andrej Mikoláš <andrej.mikolas@gmail.com>
2020-05-15 12:32:05 +0200

    Add implementation of APIWeatherInfoRepository Add dependency for reactive android

--- app/src/main/java/sk/andrejmik/weatherapp/api_repository/APIWeatherInfoRepository.kt
+++ app/src/main/java/sk/andrejmik/weatherapp/api_repository/APIWeatherInfoRepository.kt
@@ -1,13 +1,52 @@
 package sk.andrejmik.weatherapp.api_repository
 
-import android.database.Observable
+import androidx.annotation.NonNull
+import com.google.gson.Gson
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+import okhttp3.*
+import org.json.JSONObject
 import sk.andrejmik.weatherapp.objects.WeatherInfo
 import sk.andrejmik.weatherapp.repository_interface.IWeatherInfoRepository
+import java.io.IOException
+import java.util.concurrent.TimeUnit
 
 open class APIWeatherInfoRepository : IWeatherInfoRepository
 {
-    override fun get(): Observable<WeatherInfo>
+    override fun get(@NonNull cityName: String): Observable<WeatherInfo>
     {
-        TODO("Not yet implemented")
+        val requestUrl: String = APIConstants.weatherApiUrl.plus("?q=$cityName")
+        val client = OkHttpClient()
+        val request = Request.Builder().url(requestUrl).get().build()
+
+        return Observable.create<WeatherInfo> { emitter ->
+            client.newCall(request).enqueue(object : Callback
+            {
+                override fun onFailure(call: Call, e: IOException)
+                {
+                    emitter.onError(e)
+                }
+
+                override fun onResponse(call: Call, response: Response)
+                {
+                    if (response.isSuccessful)
+                    {
+                        val jsonBody = response.body?.string()
+                        val jObject = JSONObject(jsonBody).getJSONObject("data")
+                        val result = Gson().fromJson<WeatherInfo>(
+                            jObject.toString(),
+                            WeatherInfo::class.java
+                        )
+                        emitter.onNext(result)
+                        emitter.onComplete()
+                    } else
+                    {
+                        emitter.onError(IllegalStateException("${response.code}"))
+                    }
+                }
+            })
+            emitter.setCancellable { }
+        }.timeout(10, TimeUnit.SECONDS).subscribeOn(Schedulers.computation()).observeOn(Schedulers.computation())
     }
+
 }
--- app/src/main/java/sk/andrejmik/weatherapp/repository_interface/IWeatherInfoRepository.kt
+++ app/src/main/java/sk/andrejmik/weatherapp/repository_interface/IWeatherInfoRepository.kt
@@ -1,9 +1,9 @@
 package sk.andrejmik.weatherapp.repository_interface
 
-import android.database.Observable
+import io.reactivex.Observable
 import sk.andrejmik.weatherapp.objects.WeatherInfo
 
 interface IWeatherInfoRepository : IRepository<WeatherInfo>
 {
-    fun get(): Observable<WeatherInfo>
+    fun get(cityName: String): Observable<WeatherInfo>
 }
