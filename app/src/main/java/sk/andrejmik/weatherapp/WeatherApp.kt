001606a6c1246d8f125c11ee9746266d5927362b
Andrej Mikoláš <andrej.mikolas@gmail.com>
2020-05-15 22:44:55 +0200

    Add swipe to refresh ability to get new weather info

---
+++ app/src/main/java/sk/andrejmik/weatherapp/WeatherApp.kt
@@ -0,0 +1,24 @@
+package sk.andrejmik.weatherapp
+
+import android.app.Application
+import android.content.Context
+
+
+open class WeatherApp : Application()
+{
+    companion object
+    {
+        private var appInstance: Application? = null
+
+        fun getContext(): Context?
+        {
+            return appInstance!!.applicationContext
+        }
+    }
+
+    override fun onCreate()
+    {
+        super.onCreate()
+        appInstance = this
+    }
+}
